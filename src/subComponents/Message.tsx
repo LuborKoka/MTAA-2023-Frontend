500c2e625e5cde699d2a6a02822f74ae39cdf79a
Ľubor Koka <koka.lubor87@gmail.com>
2023-04-08 16:31:06 +0200

    emoji regex

--- src/subComponents/Message.tsx
+++ src/subComponents/Message.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState} from "react"
-import { useColorScheme, Dimensions, LayoutChangeEvent, View, Text, StyleSheet } from 'react-native'
+import React from "react"
+import { useColorScheme, Dimensions, View, Text, StyleSheet } from 'react-native'
 import { GREEN, WHITE, BLACK, DARKER_WHITE, LIGHTER_BLACK } from "../constants/constants"
+import emojiRegex from "emoji-regex"
 
 interface messageProps {
     isIncoming?: boolean,
@@ -10,23 +11,14 @@ interface messageProps {
 export default function Message({ isIncoming = false, content }: messageProps) {
     const isDark = useColorScheme() === 'dark'
 
-    const [width, setWidth] = useState(0)
-    const isSet = useRef(false)
+    const regex = emojiRegex()
 
     const bgColor = !isIncoming ? GREEN : isDark ? LIGHTER_BLACK : DARKER_WHITE
     const txtColor = !isIncoming ? WHITE : isDark ? WHITE : BLACK
-
-    function updateWidth(e: LayoutChangeEvent) {
-        if ( !isSet.current ) {
-            setWidth(Math.min(Dimensions.get('window').width * .7, e.nativeEvent.layout.width))
-            isSet.current = true
-        }
-    }
-
     return(
         <View style={{justifyContent: isIncoming ? 'flex-start' : 'flex-end', flexDirection: 'row'}}>
-            <View style={{...style.message, maxWidth: Dimensions.get('window').width * .7}} /* my intention is to make this view screen width * .7 wide or less, and when the text would be wider, than wrap the text, add some new lines, you know like facebook. the message content has a maximum width and if it is larger, newlines will be added. but now, new lines are added even if the text is not wider. i think the auto property is trying to format the text to multiple lines of the same width  */>
-                <Text style={{...style.messageText, color: txtColor, backgroundColor: bgColor, textAlign: 'left' /*isIncoming ? 'left' : 'right'*/}} onLayout={updateWidth} >
+            <View style={{...style.message, maxWidth: Dimensions.get('window').width * .7}} >
+                <Text style={{...style.messageText, color: txtColor, backgroundColor: bgColor, textAlign: 'left', fontSize: regex.test(content) ? 22 : 12}} >
                     {content}
                 </Text>
             </View>
